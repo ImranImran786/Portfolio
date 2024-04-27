ec0128d73530bdf89133fd071ada491e7c166eb8
Imranidrees1234 <imranidrees053@gmail.com>
2024-04-27 12:04:31 +0500

    hero content

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import "./Hero.css"
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+  return <section className="hero-container">
+    <div className="hero-content">
+        <h2>Building Digital Experiences That Inspire</h2>
+        <p>
+            passionate Fontend Developer | Transforming Ideas into Seamless 
+            and Visually Stunning Web Solutions
+        </p>
+    </div>
+    </section>
+  
 }
 
 export default Hero
