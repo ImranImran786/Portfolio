7069bb30171cd3919ad7261908bdcbaefea04cb9
Imranidrees1234 <imranidrees053@gmail.com>
2024-04-27 11:43:17 +0500

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import "./App.css"
+import Hero from "./components/Navbar/Hero/Hero"
 import { Navbar } from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
     <>
       <Navbar/>
+
+      <div className="container">
+        <Hero />
+      </div>
     </>
   )
 }
